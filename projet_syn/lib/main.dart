ed6c4049c382a492da87bfde49050eaee86b35d8
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2025-06-04 15:22:22 +0100

    new layout

--- projet_syn/lib/main.dart
+++ projet_syn/lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'screane/homePage.dart';
+import 'package:projet_syn/screen/splash/splashScreen.dart';
+
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:Homepage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: OnboardingScreen(),
     );
   }
 }
-
